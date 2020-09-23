d483d78ff383b7b0a815a5b057e1ca9be4509887
DELL <ragamayigalla13@gmail.com>
2020-09-23 11:47:00 +0530

    simple

---
+++ src/automationprograms/sample.java
@@ -0,0 +1,22 @@
+package automationprograms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sample {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("ragamayigalla@gmail.com");
+		Thread.sleep(3000);
+		driver.findElement(By.id("pass")).sendKeys("abcdefgh");
+		driver.findElement(By.name("login")).click();
+		driver.close();
+
+	}
+
+}
